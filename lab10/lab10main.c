72f27d99dec3e48ab4005a07261df78b944f6bb2
Lenny <d.yazyyykov@gmail.com>
2026-03-29 23:07:50 +1000

    lab10: wip

--- lab10/lab10main.c
+++ lab10/lab10main.c
@@ -1,17 +1,59 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <locale.h>
 
 int get_clean_str(char* str, int size);
 
+#define FIND_MAX(arr, n, max)         \
+    do                                \
+    {                                 \
+        max = arr[0];                 \
+        for (int i = 1; i < n; i++)   \
+        {                             \
+            if (arr[i] > max)         \
+            {                         \
+                max = arr[i];         \
+            }                         \
+        }                             \
+    } while (0)
+
 int main(void)
 {
-    char s[200];
 
-    printf("Введите числа: ");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+
+    char s[200];
+    int numbers[100];
+    int count = 0;
+    int max;
+
+    printf("Введите числа через пробел: ");
+
     while (get_clean_str(s, sizeof(s)));
 
+    char* token = strtok(s, " ");
+
+    while (token != NULL)
+    {
+        numbers[count] = atoi(token);
+        count++;
+        token = strtok(NULL, " ");
+    }
+
+    if (count == 0)
+    {
+        printf("Числа не введены.\n");
+        return 1;
+    }
+
+    FIND_MAX(numbers, count, max);
+
+    printf("Максимальное число: %d\n", max);
 
     return 0;
 }
@@ -32,3 +74,7 @@ int get_clean_str(char* str, int size)
 
     return 1;
 }
+
+
+//Написать программу вычисления максимального числа из введенных пользователем в виде одной строки.  
+// Использовать для этого макрос (без функции), на вход которому подается массив чисел и их количество.
