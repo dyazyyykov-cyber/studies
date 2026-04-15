e1f994992bdecb31ca46f4a6ba3b635bfa076620
Lenny <d.yazyyykov@gmail.com>
2026-04-15 23:27:45 +1000

    lab12: wip

--- lab12/lab12main.c
+++ lab12/lab12main.c
@@ -1,42 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 #include <string.h>
-#include <windows.h>
-#include <locale.h>
+#include <time.h>
 
-int main() {
+
+int main()
+{
     int year, month, day;
-    char* week[] = {
-        "Воскресенье", "Понедельник", "Вторник",
-        "Среда", "Четверг", "Пятница", "Суббота"
-    };
+    char* week[] = { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
 
-    printf("Введите дату в формате гггг.мм.дд: ");
-    scanf("%d.%d.%d", &year, &month, &day);
+    printf("Введите дату: ")
+        if (get_clean_str(str, sizeof(str)))
+        {
+            if (strcmp(str, "now") == 0)
+
+            else if (sscanf(str, "%d.%d.%d%", &year, &month, &day) == 3)
+            {
+
+            }
+            else if (sscanf(str, "%d.%d", &year, &month) == 2)
+            {
+
+            }
+            else if (sscanf(str, "%d", &year) == 1)
+            {
+
+            }
+        }
 
     struct tm t = { 0 };
     t.tm_year = year - 1900;
     t.tm_mon = month - 1;
     t.tm_mday = day;
 
-    mktime(&t);
-
-    printf("День недели: %s\n", week[t.tm_wday]);
-
-    return 0;
 }
 
 int get_clean_str(char* str, int size)
 {
     if (fgets(str, size, stdin))
     {
-        str[strcspn(str, "\n")] = '\0';
+        str[strscpn(str, "\n")] = '\0';
 
         if (str == "\0")
         {
             return 0;
         }
     }
-
     return 1;
 }
