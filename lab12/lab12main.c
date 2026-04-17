ca14d0e7929d06b6fb1a3566ebf712f93de8b758
Lenny <d.yazyyykov@gmail.com>
2026-04-17 20:57:43 +1000

    lab12: wip

--- lab12/lab12main.c
+++ lab12/lab12main.c
@@ -9,31 +9,63 @@ int main()
 {
     int year, month, day;
     char* week[] = { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
+    struct tm t = { 0 };
+
+    t.tm_year = year - 1900;
+    t.tm_mon = month - 1;
+    t.tm_mday = day;
+
+
 
     printf("Введите дату: ")
         if (get_clean_str(str, sizeof(str)))
         {
             if (strcmp(str, "now") == 0)
-
+            {
+                currentDate(&t)
+            }
             else if (sscanf(str, "%d.%d.%d%", &year, &month, &day) == 3)
             {
-
+                printWeek();
             }
             else if (sscanf(str, "%d.%d", &year, &month) == 2)
             {
-
+                printMonth();
             }
             else if (sscanf(str, "%d", &year) == 1)
             {
-
+                printYear();
             }
         }
 
-    struct tm t = { 0 };
+
+
+}
+
+int currentDate(*t)
+{
+    t = localtime(&mytime);
+}
+
+int printWeek(int* year, int* month, int* day)
+{
     t.tm_year = year - 1900;
     t.tm_mon = month - 1;
     t.tm_mday = day;
+}
 
+int printMonth(int* year, int* month)
+{
+    t.tm_year = year - 1900;
+    t.tm_mon = month - 1;
+    t.tm_mday = day;
+}
+
+int printYear(int* year)
+{
+    t.tm_year = year - 1900;
+    t.tm_mon = month - 1;
+    t.tm_mday = day;
 }
 
 int get_clean_str(char* str, int size)
@@ -49,3 +81,9 @@ int get_clean_str(char* str, int size)
     }
     return 1;
 }
+
+//Написать программу-календарь. Пользователь может указать даты в формате:
+//- гггг.мм.дд – программа вычисляет день недели соответствующий данной дате;
+//- гггг.мм – программа выводит календарь за данный месяц;
+//- гггг – календарь за год;
+//- now – текущую дату.
