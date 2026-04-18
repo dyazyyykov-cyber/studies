be52a0b33594d9f1e1c3f16833d6ff7396f69766
Lenny <d.yazyyykov@gmail.com>
2026-04-18 23:40:56 +1000

    lab12: wip

--- lab12/lab12main.c
+++ lab12/lab12main.c
@@ -9,6 +9,7 @@ int main()
 {
     int year, month, day;
     char* week[] = { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
+    char* str[80];
     struct tm t = { 0 };
 
     t.tm_year = year - 1900;
@@ -17,55 +18,59 @@ int main()
 
 
 
-    printf("Введите дату: ")
-        if (get_clean_str(str, sizeof(str)))
+    printf("Введите дату: ");
+    if (get_clean_str(str, sizeof(str)))
+    {
+        if (strcmp(str, "now") == 0)
         {
-            if (strcmp(str, "now") == 0)
-            {
-                currentDate(&t)
-            }
-            else if (sscanf(str, "%d.%d.%d%", &year, &month, &day) == 3)
-            {
-                printWeek();
-            }
-            else if (sscanf(str, "%d.%d", &year, &month) == 2)
-            {
-                printMonth();
-            }
-            else if (sscanf(str, "%d", &year) == 1)
-            {
-                printYear();
-            }
+            currentDate(&t);
         }
+        else if (sscanf(str, "%d.%d.%d%", &year, &month, &day) == 3)
+        {
+            printWeek();
+        }
+        else if (sscanf(str, "%d.%d", &year, &month) == 2)
+        {
+            printMonth();
+        }
+        else if (sscanf(str, "%d", &year) == 1)
+        {
+            printYear();
+        }
+    }
 
 
 
 }
 
-int currentDate(*t)
+int currentDate(struct tm* t)
 {
     t = localtime(&mytime);
 }
 
-int printWeek(int* year, int* month, int* day)
+int printDayWeek(struct tm *t ,int* year, int* month, int* day)
 {
-    t.tm_year = year - 1900;
-    t.tm_mon = month - 1;
-    t.tm_mday = day;
+    t->tm_year = *year - 1900;
+    t->tm_mon = *month - 1;
+    t->tm_mday = *day;
+
+    mktime(t);
+
+    return t->tm_wday;
 }
 
-int printMonth(int* year, int* month)
+int printMonth(struct tm* t, int* year, int* month)
 {
-    t.tm_year = year - 1900;
-    t.tm_mon = month - 1;
-    t.tm_mday = day;
+    t->tm_year = *year - 1900;
+    t->tm_mon = *month - 1;
+    t->tm_mday = *day;
 }
 
-int printYear(int* year)
+int printYear(struct tm* t, int* year)
 {
-    t.tm_year = year - 1900;
-    t.tm_mon = month - 1;
-    t.tm_mday = day;
+    t->tm_year = *year - 1900;
+    t->tm_mon = *month - 1;
+    t->tm_mday = *day;
 }
 
 int get_clean_str(char* str, int size)
